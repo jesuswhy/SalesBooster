ad0fa64a9a8e17742cbee97c4f1a8adc9d69ea43
jesuswhy <87827127+jesuswhy@users.noreply.github.com>
2024-10-11 10:29:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-document.querySelector('.btn-primary').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#demo').scrollIntoView({ behavior: 'smooth' });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper('.swiper-container', {
-        loop: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-});
-
-
-let currentSlide = 0;
-
-function showSlide(slideIndex) {
-    const slides = document.querySelectorAll('.testimonial-item');
-    if (slideIndex >= slides.length) {
-        currentSlide = 0;
-    } else if (slideIndex < 0) {
-        currentSlide = slides.length - 1;
-    } else {
-        currentSlide = slideIndex;
-    }
-    slides.forEach((slide, index) => {
-        slide.classList.toggle('active', index === currentSlide);
-    });
-}
-
-document.querySelector('.prev').addEventListener('click', () => {
-    showSlide(currentSlide - 1);
-});
-
-document.querySelector('.next').addEventListener('click', () => {
-    showSlide(currentSlide + 1);
-});
-
 document.addEventListener('DOMContentLoaded', function() {
     const faqItems = document.querySelectorAll('.faq-item');
 
@@ -45,13 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
         const question = item.querySelector('.faq-question');
         
         question.addEventListener('click', function() {
-            if (!item.classList.contains('active')) {
-                faqItems.forEach(el => el.classList.remove('active'));
-                item.classList.add('active');
-            } else {
-                item.classList.remove('active');
-            }
+            // Закрываем все активные элементы
+            faqItems.forEach(el => el.classList.remove('active'));
+            // Переключаем класс активного на текущем элементе
+            item.classList.toggle('active');
         });
     });
 });
-
